3c21ee7d6f9a59e2d629479a75c96d1e12aa4211
Koshi-tama <1879tk@gmail.com>
2022-02-13 21:38:39 +0900

    first commit

---
+++ src/main/java/com/tamatech/demo/service/TeacherService.java
@@ -0,0 +1,39 @@
+package com.tamatech.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tamatech.demo.model.Teacher;
+import com.tamatech.demo.repository.TeacherMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TeacherService {
+	
+	private final TeacherMapper mapper;
+	
+	public List<Teacher> selectAll() {
+		return mapper.selectAll();
+	}
+	
+	public Teacher selectByPrimaryKey(Long id) {
+		return mapper.selectByPrimaryKey(id);
+	}
+	
+	public void save(Teacher teacher) {
+		if(teacher.getId() == null) {
+			mapper.insert(teacher);
+		} else {
+			mapper.updateByPrimaryKey(teacher);
+		}
+	}
+	
+	public void deleteByPrimaryKey(Long id) {
+		mapper.deleteByPrimaryKey(id);
+	}
+}
